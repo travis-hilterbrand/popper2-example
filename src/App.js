afd8b93d90158d4c1ba8bdb578cb980b72bcab58
Travis Hilterbrand <Travis.Hilterbrand@five9.com>
2020-05-20 09:06:55 -0700

    forwardref

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React, { useState } from 'react';
 import { usePopper } from 'react-popper';
 
+const FancyButton = React.forwardRef((props, ref) => (
+  <button ref={ref} {...props} className="FancyButton" style={{height: 34}}>
+    {props.children}
+  </button>
+));
+FancyButton.defaultProps = {
+  disabled: true,
+};
+
 const Example = () => {
   const [referenceElement, setReferenceElement] = useState(null);
   const [popperElement, setPopperElement] = useState(null);
   const [arrowElement, setArrowElement] = useState(null);
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: 'bottom-start',
     modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
   });
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+
+/*
+      <button type="button" ref={setReferenceElement} onClick={handleClick}>
+        Toggle Open
+      </button>
+*/
+
   return (
     <>
-      <button type="button" ref={setReferenceElement}>
-        Reference element
-      </button>
+      <FancyButton ref={setReferenceElement} disabled={false} onClick={handleClick}>
+        Toggle Open
+      </FancyButton>
 
-      <div ref={setPopperElement} style={styles.popper} {...attributes.popper}>
-        Popper element
-        <div ref={setArrowElement} style={styles.arrow} />
-      </div>
+      { open &&
+        <div ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+          Popper element
+          <div ref={setArrowElement} style={styles.arrow} />
+        </div>
+      }
     </>
   );
 };
